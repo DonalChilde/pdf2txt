f7944c954bc306c43be3c25a580352ba9f522d31
Chad Lowe <pfmsoft@gmail.com>
2024-10-26 11:38:07 -0700

    working extract command

--- src/pdf2txt/pdf2txt_cli.py
+++ src/pdf2txt/pdf2txt_cli.py
@@ -3,10 +3,16 @@
 from pathlib import Path
 
 import typer
-from pfmsoft.snippets.file.validate_file_out import validate_file_out
 from typing_extensions import Annotated
 
+from pdf2txt.extract import extract_text_from_pdf_to_file
+from pdf2txt.snippets.sizeof_fmt import sizeof_fmt
+
 app = typer.Typer()
+
+# TODO timer for jobs
+# TODO status message jobs remaining KB remaining.
+# TODO support extracting text to command line.
 
 
 @app.command()
@@ -16,24 +22,34 @@ def extract(
     overwrite: Annotated[
         bool, typer.Option(help="Overwrite existing output file.")
     ] = False,
+    suppress_status_msgs: Annotated[
+        bool, typer.Option(help="Suppress job status messages.")
+    ] = False,
 ):
     """
     Extract text from a pdf file.
     """
+    # TODO log this instead of print
+    print(f"{locals()!r}")
     if not path_in.is_file():
         print(f"{path_in} is not a file.")
         raise typer.Exit(code=1)
-    if path_in.suffix.lower() is not "pdf":
-        print(f"input file might not be a pdf, suffix for {path_in.name} is not 'pdf'.")
+    in_suffix = path_in.suffix.lower()
+    if in_suffix != ".pdf":
+        print(
+            f"input file might not be a pdf, suffix for {path_in.name} is not "
+            f"'.pdf', it is {in_suffix}."
+        )
+        # raise typer.Exit(code=1)
+    job_message = f"Extracting text from {file_stat_msg(path_in)} to {path_out}"
+    typer.echo(job_message)
+    try:
+        extract_text_from_pdf_to_file(path_in, path_out, overwrite)
+    except ValueError as e:
+        print(e)
         raise typer.Exit(code=1)
-    if path_out.is_dir():
-        print(f"{path_out} is a directory, and must point to a file.")
-    if path_out.is_file() and not overwrite:
-        print(f"{path_out} exists and overwrite was not selected.")
-    if not validate_file_out(path_out, overwrite=overwrite, ensure_parent=True):
-        print(f"Unable to create path to {path_out}. is it writable?")
-        raise typer.Exit(code=1)
-    print(path_in, path_out, overwrite)
+
+    # assert False
 
 
 @app.command()
@@ -52,9 +68,18 @@ def extract_all(
     ] = False,
 ):
     """
-    Extract multiple files, with optional recursion
+    Extract multiple files, with optional recursion, and automatic naming of extracted files.
+
+    If recursing sub directories, the sub directory structure is reproduced
+    in the output directory. Automatic file renaming will replace the .pdf suffix
+    with the .txt suffix.
     """
-    print(path_in, path_out, overwrite, recurse)
+    # TODO log this instead of print
+    print(f"{locals()!r}")
+
+
+def file_stat_msg(file_path: Path) -> str:
+    return f"{file_path.name} - {sizeof_fmt(file_path.stat().st_size)}"
 
 
 if __name__ == "__main__":
