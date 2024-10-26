c3e3415316caf09afac9e2496155447fbab003cd
Chad Lowe <pfmsoft@gmail.com>
2024-10-26 09:39:23 -0700

    add cli runner fixture to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,13 @@
 from pathlib import Path
 
 import pytest
+from typer.testing import CliRunner
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    """Fixture for invoking command-line interfaces."""
+    return CliRunner()
 
 
 def pytest_addoption(parser):
